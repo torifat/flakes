af5efe0bc24ce1bb3e68f37cbc97f1879263218b
torifat <208544+torifat@users.noreply.github.com>
2026-07-10 14:13:13 +0000

    worktrunk: v0.66.0 → v0.67.0

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -157,10 +157,10 @@
   };
   worktrunk = {
     pname = "worktrunk";
-    version = "v0.66.0";
+    version = "v0.67.0";
     src = fetchurl {
-      url = "https://github.com/max-sixty/worktrunk/releases/download/v0.66.0/worktrunk-aarch64-apple-darwin.tar.xz";
-      sha256 = "sha256-B3NmVTB1N4Gqu22ljf71ft3UhcxYOEKTKNWfR+SXRS4=";
+      url = "https://github.com/max-sixty/worktrunk/releases/download/v0.67.0/worktrunk-aarch64-apple-darwin.tar.xz";
+      sha256 = "sha256-+UQx6WHHfZiKB+dA+A4bNOTNi3CFOvE/4wVitovF+X8=";
     };
   };
 }
