abfc341bd8d9ab702bf2e0d0cf0cf69669667fea
torifat <208544+torifat@users.noreply.github.com>
2026-08-02 13:11:05 +0000

    mole: 1.48.1 → 1.49.0

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -56,10 +56,10 @@
   };
   mole = {
     pname = "mole";
-    version = "1.48.1";
+    version = "1.49.0";
     src = fetchurl {
-      url = "https://github.com/tw93/mole/archive/refs/tags/V1.48.1.tar.gz";
-      sha256 = "sha256-N03NyYHQWBzfUAcxH7W/TP4yatX+Knc1/8RKP3yRsEk=";
+      url = "https://github.com/tw93/mole/archive/refs/tags/V1.49.0.tar.gz";
+      sha256 = "sha256-R1XlloZxeGbP4dazoBvAiiTwqbfWhLs4heLBGpTJkyA=";
     };
   };
   open-super-whisper = {
