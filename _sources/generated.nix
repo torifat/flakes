000ec69f2189ef056bb3d498026269c632cfd330
torifat <208544+torifat@users.noreply.github.com>
2026-08-16 06:32:13 +0000

    mole: 1.50.0 → 1.51.0

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -56,10 +56,10 @@
   };
   mole = {
     pname = "mole";
-    version = "1.50.0";
+    version = "1.51.0";
     src = fetchurl {
-      url = "https://github.com/tw93/mole/archive/refs/tags/V1.50.0.tar.gz";
-      sha256 = "sha256-CORXWuk2u3mlVU9Ym7hKMMbW64m29cdK4AIrrlQl3Dk=";
+      url = "https://github.com/tw93/mole/archive/refs/tags/V1.51.0.tar.gz";
+      sha256 = "sha256-nV5DQMbl/Pp/c98w0qRjY2ACl/jWrhWbCNT890AzGFI=";
     };
   };
   open-super-whisper = {
